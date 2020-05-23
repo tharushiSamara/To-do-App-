04f58dd4ef8a4ad939b4de5838879027c8b6b260
tharushiSamara <tharushisamara157@gmail.com>
2020-05-23 22:32:48 +0530

    Using MongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,22 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'some coding'}];
+//Connect to the database
+mongoose.connect('mongodb+srv://test:test@todo-xb8bp.mongodb.net/test?retryWrites=true&w=majority'); 
+
+//create a schema- this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+//     if (err) throw err;
+//     console.log('item saved');
+// })
+
+
+// var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'some coding'}];
 
 //parser the storing data- POST handler (middleware)
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -8,10 +24,16 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 module.exports = function(app){
 
 app.get('/todo', function(req, res){
-    res.render('todo', {todos: data});
+    //get data from mongodb and pass it to view
+    Todo.find({} ,function(err, data){
+        if (err) throw err;
+        res.render('todo', {todos: data});
+    });
+    
 });
 
 app.post('/todo', urlencodedParser, function(req, res){
+   
     data.push(req.body);
     res.json(data);
 });
